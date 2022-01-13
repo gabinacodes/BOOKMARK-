d8049315b42ee5baef4610ce2c75137dadd56485
gabinacodes <badruraiah@gmail.com>
2022-01-13 17:35:03 +0100

    frst push

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("div11").style.display = "none"
+document.getElementById("a1").onclick = function(){
+    document.getElementById("a1").style.borderBottom = "5px solid red"
+
+    document.getElementById("div11").style.display = "block"
+}
